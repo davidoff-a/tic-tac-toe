9d08e8836432eb4619ff51f539fa9386cc8b7b1c
Davydau Akiaksei <davidoff.aleksei@gmail.com>
2021-08-29 09:01:49 +0300

    feat: add methods NoMoreTurns, isFinished, getWinner. But only noMoreTurns passes the tests

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -6,7 +6,6 @@ class TicTacToe {
       [, , ,],
     ];
     this.currentSymbol = "x";
-    // this.turns=0;
   }
 
   getCurrentPlayerSymbol() {
@@ -14,7 +13,7 @@ class TicTacToe {
   }
 
   nextTurn(rowIndex, columnIndex) {
-    if (!this.getFieldValue(rowIndex,columnIndex)) {
+    if (!this.getFieldValue(rowIndex, columnIndex)) {
       if (this.currentSymbol === "x") {
         this.currentSymbol = "o";
       } else {
@@ -22,19 +21,63 @@ class TicTacToe {
       }
       this.gameField[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
     }
-    
   }
 
-  isFinished() {}
+  isFinished() {
+    if (this.getWinner()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-  getWinner() {}
+  getWinner() {
+    for (let i = 0; i < 3; i++) {
+      if (
+        this.gameField[i][0] === this.gameField[i][1] &&
+        this.gameField[i][0] === this.gameField[i][2]
+      ) {
+        return this.gameField[i][0];
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      if (
+        this.gameField[0][i] === this.gameField[1][i] &&
+        this.gameField[0][i] === this.gameField[2][i]
+      ) {
+        return this.gameField[0][i];
+      }
+    }
+    if (
+      this.gameField[0][0] === this.gameField[1][1] &&
+      this.gameField[0][0] === this.gameField[2][2]
+    ) {
+      return this.gameField[0][0];
+    }
+    if (
+      this.gameField[1][1] === this.gameField[0][2] &&
+      this.gameField[1][1] === this.gameField[2][0]
+    ) {
+      return this.gameField[1][1];
+    }
+    return null;
+  }
 
-  noMoreTurns() {}
+  noMoreTurns() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (!this.gameField[i][j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 
   isDraw() {}
 
   getFieldValue(rowIndex, colIndex) {
-    if (!this.gameField[rowIndex][colIndex]){
+    if (!this.gameField[rowIndex][colIndex]) {
       return null;
     } else {
       return this.gameField[rowIndex][colIndex];
